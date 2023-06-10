67860a7747baa298ef53cf6d7ccdb0f56008c9c7
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-10 16:58:23 +0530

    add todo logic

---
+++ src/components/Todoitem.jsx
@@ -0,0 +1,28 @@
+const TodoItem = ({ itemProp, setTodos }) => {
+  
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+    prevState.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    })
+  );
+  };
+  
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={itemProp.completed}
+        onChange={() => handleChange(itemProp.id)}
+      />
+      {itemProp.title}
+    </li>
+  );
+};
+export default TodoItem;
