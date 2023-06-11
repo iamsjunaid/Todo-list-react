b50d38f31127204df01124e04b492c34c4123cff
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-06-11 11:16:06 +0530

    add styles

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -1,3 +1,4 @@
+import styles from '@/styles/TodoItem.module.css';
 /* eslint-disable react/prop-types */
 const TodoItem = ({ itemProp, setTodos, delTodo }) => {
   const handleChange = (id) => {
@@ -14,15 +15,26 @@ const TodoItem = ({ itemProp, setTodos, delTodo }) => {
     );
   };
 
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+
   return (
-    <li>
-      <input
-        type="checkbox"
-        checked={itemProp.completed}
-        onChange={() => handleChange(itemProp.id)}
-      />
-      <button onClick={() => delTodo(itemProp.id)}>Delete</button>
-      {itemProp.title}
+    <li className={styles.item}>
+      <div className={styles.content}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+        <span style={itemProp.completed ? completedStyle : null}>
+          {itemProp.title}
+        </span>
+      </div>
     </li>
   );
 };
